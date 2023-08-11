0997f289ec642127a09e2b876179060f09db4761
jayadamsmorgan <applejay1802@gmail.com>
2023-08-11 17:19:08 -0400

    Initial Commit

---
+++ HomeClient-iOS/HomeClient_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeClient_iOSApp.swift
+//  HomeClient-iOS
+//
+//  Created by user on 8/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeClient_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
